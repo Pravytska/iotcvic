d1b143105fd5bfee879e8416d2b78804332d28de
Pravytska <114649880+Pravytska@users.noreply.github.com>
2022-11-10 09:26:41 +0100

    index2.php

--- index2.php
+++ index2.php
@@ -1,13 +1,13 @@
 <?php
         echo '<h1>getParameters page</h1>';
 
-        //echo '<h2> https://phpipaiot.azurewebsites.net/index2.php?a=10&b=2 </h2>';
+        //echo '<h2> https://iotcvic.azurewebsites.net/index2.php?a=10&b=5 </h2>';
         
         $sn1 = $_GET["a"];
         $sn2 = $_GET["b"];
         
         $text = "a=" . $sn1 . " b=" . $sn2;
-        $sum = $sn1 + $sn2;
+        $sum = $sn1 + $sn2
         
         echo $text;
         echo "<br>";
