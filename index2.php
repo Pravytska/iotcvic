34defeada8311ba3f6f6ba0515ae9cbf15d254ac
Pravytska <114649880+Pravytska@users.noreply.github.com>
2022-11-10 09:21:20 +0100

    add index2.php

---
+++ index2.php
@@ -0,0 +1,15 @@
+<?php
+        echo '<h1>getParameters page</h1>';
+
+        //echo '<h2> https://phpipaiot.azurewebsites.net/index2.php?a=10&b=2 </h2>';
+        
+        $sn1 = $_GET["a"];
+        $sn2 = $_GET["b"];
+        
+        $text = "a=" . $sn1 . " b=" . $sn2;
+        $sum = $sn1 + $sn2;
+        
+        echo $text;
+        echo "<br>";
+        echo "Sum:" . $sum;
+    ?>
