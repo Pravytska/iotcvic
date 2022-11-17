657865bd70163770af71d9bda6160709edaafb83
Pravytska <114649880+Pravytska@users.noreply.github.com>
2022-11-18 00:34:22 +0100

    add forma.php

--- index4.php
+++ index4.php
@@ -1,11 +1,5 @@
-<html>
-<body>
+<?php
 
-<form action="welcome.php" method="post">
-Name: <input type="text" name="name"><br>
-E-mail: <input type="text" name="email"><br>
-<input type="submit">
-</form>
 
-</body>
-</html>
+
+?>
