b47ee3aca10a3a4d4dbc1373f7cb0a1fdc13c812
Pravytska <114649880+Pravytska@users.noreply.github.com>
2022-11-20 10:48:05 +0100

    phone number try

--- forma.php
+++ forma.php
@@ -1,76 +1,31 @@
+<form name="form1" method="post" action="">
+<table width="100%"  border="1" align="center" cellpadding="0" cellspacing="0" bordercolor="#0033FF" bgcolor="#DEE3EF" >
+<tr><td width="100%">
+<table width="60%" height="25%"  border="0" align="center" cellpadding="0" cellspacing="0" bgcolor="#DEE3ED">
+<caption align="left" class="heading">
+    Phone Number Validation
+</caption>
+<tr>
+<td width="46%" class="text"><div align="right">Enter Your Phone Number </div>
+<td width="5%"><input name="txtisdcode" type="text" class="textbox"  size="7" value="<?php echo $intisd; ?>" maxlength="5">
+<td width="10%"><input name="txtcitycode" type="text" class="textbox"  size="7" value="<?php echo $intccode;?>" maxlength="5">
+<td width="26%"><input name="txtphone" type="text" class="textbox" value="<?php echo $intphone;?>" maxlength="20" >
+</tr>
+<tr>
+<td colspan="4"><div align="center">
+<br>
+<input type="submit" name="Submit" value="Submit">
+<input name="hidSubmit" type="hidden" id="hidSubmit" value="true">
+</div></td>
+</tr>
+</table>
+<div align="center"></div>
+</td>
+</tr>
+</table>
+</form>
 <?php
-  
-if($_POST) {
-    $visitor_name = "";
-    $visitor_email = "";
-    $email_title = "";
-    $concerned_department = "";
-    $visitor_message = "";
-    $email_body = "<div>";
-      
-    if(isset($_POST['visitor_name'])) {
-        $visitor_name = filter_var($_POST['visitor_name'], FILTER_SANITIZE_STRING);
-        $email_body .= "<div>
-                           <label><b>Visitor Name:</b></label>&nbsp;<span>".$visitor_name."</span>
-                        </div>";
-    }
- 
-    if(isset($_POST['visitor_email'])) {
-        $visitor_email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['visitor_email']);
-        $visitor_email = filter_var($visitor_email, FILTER_VALIDATE_EMAIL);
-        $email_body .= "<div>
-                           <label><b>Visitor Email:</b></label>&nbsp;<span>".$visitor_email."</span>
-                        </div>";
-    }
-      
-    if(isset($_POST['email_title'])) {
-        $email_title = filter_var($_POST['email_title'], FILTER_SANITIZE_STRING);
-        $email_body .= "<div>
-                           <label><b>Reason For Contacting Us:</b></label>&nbsp;<span>".$email_title."</span>
-                        </div>";
-    }
-      
-    if(isset($_POST['concerned_department'])) {
-        $concerned_department = filter_var($_POST['concerned_department'], FILTER_SANITIZE_STRING);
-        $email_body .= "<div>
-                           <label><b>Concerned Department:</b></label>&nbsp;<span>".$concerned_department."</span>
-                        </div>";
-    }
-      
-    if(isset($_POST['visitor_message'])) {
-        $visitor_message = htmlspecialchars($_POST['visitor_message']);
-        $email_body .= "<div>
-                           <label><b>Visitor Message:</b></label>
-                           <div>".$visitor_message."</div>
-                        </div>";
-    }
-      
-    if($concerned_department == "billing") {
-        $recipient = "billing@domain.com";
-    }
-    else if($concerned_department == "marketing") {
-        $recipient = "marketing@domain.com";
-    }
-    else if($concerned_department == "technical support") {
-        $recipient = "tech.support@domain.com";
-    }
-    else {
-        $recipient = "contact@domain.com";
-    }
-      
-    $email_body .= "</div>";
- 
-    $headers  = 'MIME-Version: 1.0' . "\r\n"
-    .'Content-type: text/html; charset=utf-8' . "\r\n"
-    .'From: ' . $visitor_email . "\r\n";
-      
-    if(mail($recipient, $email_title, $email_body, $headers)) {
-        echo "<p>Thank you for contacting us, $visitor_name. You will get a reply within 24 hours.</p>";
-    } else {
-        echo '<p>We are sorry but the email did not go through.</p>';
-    }
-      
-} else {
-    echo '<p>Something went wrong</p>';
+return;
 }
 ?>
+    
