3c54f5f1a6f56f1def056b1a6776e80e0484c172
Pravytska <114649880+Pravytska@users.noreply.github.com>
2022-12-21 23:03:54 +0100

    try to work with files

--- esp.php
+++ esp.php
@@ -18,6 +18,39 @@
     </style>
 </head>
 <body>
+    <?php
+        function loadDataFromFile($file)
+        {
+          if (!file_exists($file))
+              throw new Exception("Ошибка: файл $file не существует!");
+          if (!filesize($file))
+              throw new Exception("Файл $file пустой!");
+          // Открываем поток и получаем его дескриптор
+          $f = fopen($file, "r");
+          // В переменную $content запишем то, что прочитали из файла
+          $content = fread($f, filesize ($file));
+          // Заменяем переносы строки в файле на тег BR. Заменить можно что угодно
+          //$content = str_replace("\r\n","<br>", $content);
+          // Закрываем поток
+          fclose ($f);
+          // Возвращаем содержимое
+          return $content;
+        }
+         
+        // Файл, с которым работаем
+        $file = __DIR__.'/file.txt';
+         
+        // Выводим информацию из файла
+        try {
+          echo loadDataFromFile($file);    
+        } catch (Exception $e) {
+          echo $e->getMessage();
+        }
+    ?>
     <h1>Smart auto</h1>
+
+
+
+
 </body>
 </html>
