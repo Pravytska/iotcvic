fe621fe945f973e6da020fdb629000191e15847d
Pravytska <114649880+Pravytska@users.noreply.github.com>
2022-12-21 22:18:40 +0100

    esp

--- esp.php
+++ esp.php
@@ -2,7 +2,19 @@
 <html lang="sk">
 <head>
     <style>
+        body {
+            background-image: url("https://images.freeimages.com/images/large-previews/a3b/website-rays-background-pattern-1637863.png");
+            font-family: 'Raleway', sans-serif;
+        }
+        .label {
+            display: inline-block;
+            width: 100px;
+            font-size: 16px;
+        }
 
+        .error {
+            color: #FF0000;
+        }
     </style>
 </head>
 <body>
